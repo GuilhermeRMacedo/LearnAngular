1ad6f7143986ad6b1741f4231244319d9610e09b
Guilherme Macedo <guilhermericardord@gmail.com>
2018-10-09 16:48:00 -0300

    Filtering a List

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './Product';
 
+
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
@@ -12,8 +13,17 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = "cart";
 
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] = [
         {
             "productId": 1,
@@ -67,11 +77,22 @@ export class ProductListComponent implements OnInit {
         }
     ]
 
+    constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage
     }
 
     ngOnInit(): void {
         console.log("In OnInit");
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct)=>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) != -1);
     }
 }
